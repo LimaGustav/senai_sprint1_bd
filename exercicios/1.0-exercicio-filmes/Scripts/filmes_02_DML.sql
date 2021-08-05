3c231da980b102d8531d656872cbfa0f72450498
LimaGustav <lima.gustavo100@hotmail.com>
2021-08-05 17:22:09 -0300

    1.1 DQL

---
+++ exercicios/1.0-exercicio-filmes/Scripts/filmes_02_DML.sql
@@ -0,0 +1,7 @@
+INSERT INTO GENERO (nomeGenero)
+VALUES ('Ação'),('Comédia'),('Romance')
+GO
+
+INSERT INTO FILME (idGenero,nomeFilme)
+VALUES (1,'Minha mãe é uma peça'),(2,'Limitless')
+GO
---
+++ exercicios/1.1-exercicio-pessoas/Scripts/empresa_02_DML.sql
@@ -0,0 +1,11 @@
+INSERT INTO PESSOA (nomePessoa,CNH)
+VALUES ('Gustavo','12345654321'),('Lucas','65432112345')
+GO
+
+INSERT INTO TELEFONE (idPessoa,numeroTelefone)
+VALUES (1,'11932259054'), (2,'1124311934'),(2,'40028922')
+GO
+
+INSERT INTO EMAIL (idPessoa,endEmail)
+VALUES (2,'g@hotmail.com'),(1,'ol@hotmail.com')
+GO
